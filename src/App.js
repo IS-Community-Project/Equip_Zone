0cdf48dd4b9a4c5d0b71e6e9085b11a6cac8eea5
Nethmi-Vaas <anuvaas15@gmail.com>
2025-01-11 21:44:23 +0530

    dashboard_1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 
+
 import Dashboard2 from './pages/admin/dashboard2'; 
 import Dashboard3 from './pages/admin/dashboard3'; 
 import RemainItems1 from './pages/admin/remain_items1';
 import BorrowedItems2 from './pages/admin/borrowed_items2';
 import Sidebar from './pages/admin/sidebar';
 import RegistrationPage from './pages/user/Register';
+import Dashboard1 from './pages/admin/dashboard1'; 
 
 
 function App() {
   return (
     <div className="APP">
-      <Dashboard3/>
+      <Dashboard1/>
     </div>
   );
 }
---
+++ src/pages/admin/dashboard1.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Dashboard1.css';
+
+
+const Dashboard1 = () => {
+  const sportsItems = [
+    { name: 'Cricket', image: 'path-to-cricket-image.png' },
+    { name: 'Netball', image: 'path-to-netball-image.png' },
+    { name: 'Football', image: 'path-to-football-image.png' },
+    { name: 'Badminton', image: 'path-to-badminton-image.png' },
+    { name: 'Table Tennis', image: 'path-to-table-tennis-image.png' },
+    { name: 'Athletics', image: 'path-to-athletics-image.png' },
+  ];
+
+  return (
+    <div className="dashboard">
+      <div className="header">
+        <div className="title">
+          <h2>Sports Items</h2>
+          <p className="availability">Availability (over-all)</p>
+        </div>
+        <div className="search-bar">
+          <input type="text" placeholder="Search items..." />
+        </div>
+      </div>
+      <div className="sports-grid">
+        {sportsItems.map((item, index) => (
+          <div className="sports-item" key={index}>
+            <div className="name-box">{item.name}</div>
+            <img src={item.image} alt={item.name} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard1;
